4a3a062abb77c6e2e361a14f3e015f452882bf3b
Quentin Barrand <quentin.barrand@swisscom.com>
2021-02-25 19:19:34 +0100

    Add a parser.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/qbarrand/ghc-2021-qualification
+
+go 1.16
--- main.go
+++ main.go
@@ -49,5 +49,83 @@ func main() {
 }
 
 func process(in, outdir string, logger *log.Logger) {
-	
+	sim, err := Parse(in)
+	if err != nil {
+		logger.Fatalf("Failed to parse %s: %v", in, err)
+	}
+
+	logger.Printf("%s: %+v", in, sim)
+
+	_ = sim
+}
+
+func Parse(in string) (*Simulation, error) {
+	s := Simulation{}
+
+	fd, err := os.Open(in)
+	if err != nil {
+		return nil, err
+	}
+
+	var (
+		streets int
+		cars    int
+	)
+
+	if _, err := fmt.Fscanf(fd, "%d %d %d %d %d", &s.Duration, &s.Intersections, &streets, &cars, &s.Bonus); err != nil {
+		return nil, fmt.Errorf("could not parse header: %v", err)
+	}
+
+	s.CarPaths = make([][]string, 0, cars)
+	s.Streets = make(map[string]*Street, streets)
+
+	for i := 0; i < streets; i++ {
+		st := Street{}
+
+		if _, err := fmt.Fscanf(fd, "%d %d %s %d", &st.Begin, &st.End, &st.Name, &st.Time); err != nil {
+			return nil, fmt.Errorf("could not parse street %d: %v", i, err)
+		}
+
+		s.Streets[st.Name] = &st
+	}
+
+	for i := 0; i < cars; i++ {
+		var (
+			name     string
+			nstreets int
+		)
+
+		if _, err := fmt.Fscanf(fd, "%d", &nstreets); err != nil {
+			return nil, fmt.Errorf("could not parse the number of streets: %v", err)
+		}
+
+		path := make([]string, 0, nstreets)
+
+		for j := 0; j < nstreets; j++ {
+			if _, err := fmt.Fscanf(fd, "%s", &name); err != nil {
+				return nil, fmt.Errorf("could not parse street %d: %v", j, err)
+			}
+
+			path = append(path, name)
+		}
+
+		s.CarPaths = append(s.CarPaths, path)
+	}
+
+	return &s, nil
+}
+
+type Simulation struct {
+	Duration      int
+	Intersections int
+	Streets       map[string]*Street
+	CarPaths      [][]string
+	Bonus         int
+}
+
+type Street struct {
+	Begin int
+	End   int
+	Name  string
+	Time  int
 }
